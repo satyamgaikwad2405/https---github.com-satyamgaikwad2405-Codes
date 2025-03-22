84c35b64fb7604b1841388700e4d6ea7e3af3cbc
satyamgaikwad2405 <satyamgaikwad2405@gmail.com>
2025-03-22 22:24:03 +0530

    c code

---
+++ C++/vivek.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Satyam {
+    public static void main(String satyam[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num1=");
+        int x = sc.nextInt();
+
+        System.out.print("Enter num2=");
+        int y = sc.nextInt();
+
+        System.out.println(x+y);
+    }
+}
